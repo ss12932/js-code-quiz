59a5bfdf2853559cf70cfa595344c57531a5abe7
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-02 00:34:10 +0100

    working on js - render q sctn

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,66 +5,75 @@
 const startBtn = document.getElementById("start-btn");
 const main = document.getElementById("main");
 const infoSctn = document.getElementById("info-section");
+const nextBtn = document.getElementById("next-btn");
+let questionIndex = 0;
 
 //callback Fns
+const initLS = function () {
+  const highScoreLS = JSON.parse(localStorage.getItem("highScore"));
+
+  //if not exist, set empty string
+  if (!highScoreLS) {
+    localStorage.setItem("highScore", JSON.stringify([]));
+  }
+};
 
 const removeStartButton = function () {
   startBtn.remove();
 };
 
 const renderInfoSection = function () {
-  // //create Section
-  // const section = document.createElement("section");
-  // section.setAttribute("class", "section-container");
-  // section.setAttribute("id", "info-section");
-  // //create Header
-  // const header = document.createElement("header");
-  // header.setAttribute("class", "header");
-  // //create JS FA logo
-  // const i1 = document.createElement("i");
-  // i1.setAttribute("class", "fa-brands fa-js");
-  // //create Highscore Crown logo
-  // const i2 = document.createElement("i");
-  // i2.setAttribute("class", "fa-solid fa-crown fa-xl");
-  // //create h1
-  // const h1 = document.createElement("h1");
-  // h1.setAttribute("class", "content-section-title");
-  // h1.textContent = "Code Quiz";
-  // //create div wrapper
-  // const hsDiv = document.createElement("div");
-  // hsDiv.setAttribute("class", "hs-btn");
-  // //create anchor
-  // const anchor = document.createElement("a");
-  // anchor.setAttribute("href", "highscore.html");
-  // anchor.textContent = i2;
-  // //create ordered list
-  // const ruleOl = document.createElement("ol");
-  // ruleOl.setAttribute("class", "rule-list");
-  // const li1 = document.createElement("li");
-  // li1.setAttribute("class", "rule-list");
-  // const li2 = document.createElement("li");
-  // li2.setAttribute("class", "rule-list");
-  // const li3 = document.createElement("li");
-  // li3.setAttribute("class", "rule-list");
-  // const li4 = document.createElement("li");
-  // li4.setAttribute("class", "rule-list");
-  // //append
-  // ruleOl.append(li1, li2, li3, li4);
-  // hsDiv.append(anchor);
-  // header.append(h1, hsDiv);
-  // section.append(header);
-  // main.append(section);
-
   infoSctn.classList.remove("section-hide");
   infoSctn.classList.add("section-active");
 };
 
+const qSctnRender = function () {
+  //get current question. 1st question is 0, array zero based
+  const currentQuestion = questions[questionIndex];
+
+  //create section
+  const section = document.createElement("section");
+  section.setAttribute("class", "section-container");
+  section.setAttribute("id", "question-container");
+
+  //create header
+  const header = document.createElement("header");
+  header.setAttribute("class", "header-container");
+
+  //create header div
+  const headerDiv = document.createElement("div");
+  headerDiv.setAttribute("class", "header-container");
+
+  //create h1 title
+  const h1appTitle = document.createElement("h1");
+  h1appTitle.setAttribute("class", "application-title");
+  h1appTitle.textContent = "Code Quiz";
+
+  //create font awesome js logo
+  const jsLogo = document.createElemnet("i");
+  jsLogo.setAttribute("class", "fa-brands fa-js");
+  //create div hud wrapper
+  const divHudWrapper = document.createElement("div");
+  divHudWrapper.setAttribute("class", "hud-wrapper");
+  //create Highscore hud div
+  const divHSHud = document.createElement("div");
+  divHSHud.setAttribute("class", "hs-hud");
+
+  const divHSHud = document.createElement("div");
+};
+
 const infoSectionLoad = function () {
+  //initialise Local Storage
+  initLS();
+
   //remove Start Button
   removeStartButton();
 
   //render info section
   renderInfoSection();
+
+  //add event listener on next button
+  nextBtn.addEventListener("click", qSctnRender);
 };
 
 //Event Listeners AEL
